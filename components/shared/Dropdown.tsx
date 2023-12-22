c44f214093767c10b6d25c2a1c53eaf6d843816b
Muhammad Faizan Hasnaat <faizanhasnaat13@gmail.com>
2023-12-22 23:17:13 +0500

    implemented file uploading

--- components/shared/Dropdown.tsx
+++ components/shared/Dropdown.tsx
@@ -1,6 +1,4 @@
-'use client'
-
-import { useState } from "react"
+import { startTransition, useState } from "react"
 
 import {
     Select,
@@ -21,6 +19,7 @@ import {
     AlertDialogTrigger,
 } from "@/components/ui/alert-dialog"  
 import { ICategory } from "@/lib/database/models/category.model"
+import { Input } from "../ui/input"
 
 type DropdownProps = {
     value?: string
@@ -30,6 +29,11 @@ type DropdownProps = {
 const Dropdown = ({ value, onChangeHandler }: DropdownProps) => {
 
     const [categories, setCategories] = useState<ICategory[]>([])
+    const [newCategory, setNewCategory] = useState('')
+
+    const handleAddCategory = () => {
+
+    }
 
     return (
         <Select onValueChange={onChangeHandler} defaultValue={value }>
@@ -47,15 +51,14 @@ const Dropdown = ({ value, onChangeHandler }: DropdownProps) => {
                     <AlertDialogTrigger className="p-medium-14 flex w-full rounded-sm py-3 pl-8 text-primary-500 hover:bg-primary-50 focus:text-primary-500">Open</AlertDialogTrigger>
                     <AlertDialogContent className="bg-white">
                         <AlertDialogHeader>
-                            <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
+                            <AlertDialogTitle>New Category</AlertDialogTitle>
                             <AlertDialogDescription>
-                                This action cannot be undone. This will permanently delete your account
-                                and remove your data from our servers.
+                                <Input type="text" placeholder="Category Name" className="input-field mt-3" onChange={(e) => setNewCategory(e.target.value)} />
                             </AlertDialogDescription>
                         </AlertDialogHeader>
                         <AlertDialogFooter>
                             <AlertDialogCancel>Cancel</AlertDialogCancel>
-                            <AlertDialogAction>Continue</AlertDialogAction>
+                            <AlertDialogAction onClick={() => startTransition(handleAddCategory)}>Add</AlertDialogAction>
                         </AlertDialogFooter>
                     </AlertDialogContent>
                 </AlertDialog>
--- components/shared/EventForm.tsx
+++ components/shared/EventForm.tsx
@@ -8,16 +8,17 @@ import { Button } from "@/components/ui/button"
 import {
   Form,
   FormControl,
-  FormDescription,
   FormField,
   FormItem,
-  FormLabel,
   FormMessage,
 } from "@/components/ui/form"
 import { Input } from "@/components/ui/input"
 import { eventFormSchema } from '@/lib/validator'
 import { eventDefaultValues } from '@/constants'
 import Dropdown from './Dropdown'
+import { Textarea } from '../ui/textarea'
+import { FileUploader } from './FileUploader'
+import { useState } from 'react'
 
 const formSchema = z.object({
   username: z.string().min(2, {
@@ -31,6 +32,8 @@ type EventFormProps = {
 }
 
 const EventForm = ({ userId, type }: EventFormProps) => {
+
+  const [files, setFiles] = useState<File[]>([])
 
   const initialValues = eventDefaultValues;
 
@@ -67,6 +70,38 @@ const EventForm = ({ userId, type }: EventFormProps) => {
               <FormItem className='w-full'>
                 <FormControl>
                   <Dropdown onChangeHandler={field.onChange} value={field.value} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+        </div>
+
+        <div className='flex flex-col gap-5 md:flex-row'>
+          <FormField
+            control={form.control}
+            name="description"
+            render={({ field }) => (
+              <FormItem className='w-full'>
+                <FormControl className='h-72'>
+                  <Textarea placeholder="Description" {...field} className='textarea rounded-2xl' />
+                </FormControl> 
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+
+          <FormField
+            control={form.control}
+            name="imageUrl"
+            render={({ field }) => (
+              <FormItem className='w-full'>
+                <FormControl className='h-72'>
+                  <FileUploader 
+                    onFieldChange={field.onChange}
+                    imageUrl={field.value}
+                    setFiles={setFiles}
+                  />
                 </FormControl>
                 <FormMessage />
               </FormItem>
